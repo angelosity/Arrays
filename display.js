395ee8a0aa079cc08c841b1d222faecb9eafc872
angelosity <angelmontilla12@hotmail.com>
2018-10-18 12:23:19 -0400

    first commit

---
+++ display.js
@@ -0,0 +1,16 @@
+
+function display(){
+
+	
+	let name1 =document.getElementById("fname").value;
+
+	let name2 = document.getElementById("lname").value;
+	document.getElementById("display").innerHTML = "welcome  "  + name1 +" "+ name2;
+	console.log(name1 + name2);
+
+
+}
+
+//function getFormValue()
+
+// document.getElementById("display").innerHTML= getFormValue();
